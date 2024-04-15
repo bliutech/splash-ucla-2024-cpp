aba284c64cb528a86a5d0ccbcb336367108f2a56
Benson Liu <bensonhliu@gmail.com>
2024-04-15 09:36:00 +0000

    feat(timing): adjusted timing to allow player and alien to move 'at the same time'

--- pixel-panic.cpp
+++ pixel-panic.cpp
@@ -25,8 +25,10 @@ using namespace std;
 
 int main() {
   int count = 0;
+  int i = 0;
   Game g = Game();
-  g.board[0][0] = GRAPHICS_PLAYER;
+  int px = 0, py = 0;
+  g.board[px][py] = GRAPHICS_PLAYER;
   clearScreen();
   printScreen(g);
   cout << "Count: " << count << endl;
@@ -43,13 +45,21 @@ int main() {
     fd_set set;
     struct timeval tv;
 
-    tv.tv_sec = 1;
-    tv.tv_usec = 0;
+    tv.tv_sec = 0;
+    tv.tv_usec = 50000;
 
     FD_ZERO(&set);
     FD_SET(fileno(stdin), &set);
 
     count += 1;
+    clearScreen();
+    printScreen(g);
+    cout << "Count: " << count << endl;
+
+    // Dummy alien.
+    g.board[i / BOARD_WIDTH][i % BOARD_HEIGHT] = GRAPHICS_NEUTRAL;
+    i = count % (BOARD_WIDTH * BOARD_HEIGHT);
+    g.board[i / BOARD_WIDTH][i % BOARD_HEIGHT] = GRAPHICS_ALIEN;
 
     int res = select(fileno(stdin) + 1, &set, NULL, NULL, &tv);
     if (res < 0) {
@@ -58,13 +68,9 @@ int main() {
     } else if (res == 0) {
       cout << "Timeout" << endl;
       // Currently, this sleep is not necessary but maybe need to handle this
-      // later on. sleep(5000000);
+      // later on. usleep(5000000);
       continue;
     }
-
-    clearScreen();
-    printScreen(g);
-    cout << "Count: " << count << endl;
 
     char c;
     cout << "Input available." << endl;
@@ -75,6 +81,52 @@ int main() {
     // TODO: Make sure alien movement is different than player movement.
     // Currently, each cycle through the loop is essentially determined by
     // whether or not a player made a keypress.
+    switch (c) {
+      case 'A':  // Up key
+      case 'w':
+        if (py <= 0) {
+          py = 0;
+          break;
+        }
+        g.board[py][px] = GRAPHICS_NEUTRAL;
+        g.board[py - 1][px] = GRAPHICS_PLAYER;
+        py--;
+        break;
+      case 'D':  // Left key
+      case 'a':
+        if (px <= 0) {
+          px = 0;
+          break;
+        }
+        g.board[py][px] = GRAPHICS_NEUTRAL;
+        g.board[py][px - 1] = GRAPHICS_PLAYER;
+        px--;
+        break;
+      case 'C':  // Right key
+      case 'd':
+        if (px >= BOARD_WIDTH - 1) {
+          px = BOARD_WIDTH - 1;
+          break;
+        }
+        g.board[py][px] = GRAPHICS_NEUTRAL;
+        g.board[py][px + 1] = GRAPHICS_PLAYER;
+        px++;
+        break;
+      case 'B':
+      case 's':  // Down key
+        if (py >= BOARD_HEIGHT - 1) {
+          py = BOARD_HEIGHT - 1;
+          break;
+        }
+        g.board[py][px] = GRAPHICS_NEUTRAL;
+        g.board[py + 1][px] = GRAPHICS_PLAYER;
+        py++;
+        break;
+      default:
+        break;
+    }
+
+    usleep(100000);
   }
 
   tcsetattr(fileno(stdin), TCSANOW, &oldSettings);
